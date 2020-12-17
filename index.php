3ebb12a81eadd5109c0aaaa81228823f7e27dae5
Marco <marco.chianese93@gmail.com>
2020-12-17 17:26:10 +0100

    stampa dinamica array (da fixare) .

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
+<?php
+$matches = [
+    [
+        'home' => 'Milano',
+        'visitors' => 'Napoli',
+        'home-score' => 50,
+        'visitors-score' => 50,
+    ],
+    [
+        'home' => 'Olimpia Milano',
+        'visitors' => 'Cantù',
+        'home-score' => 50,
+        'visitors-score' => 50,
+    ],
+    [
+        'home' => 'Milano',
+        'visitors' => ' Virtus Napoli',
+        'home-score' => 50,
+        'visitors-score' => 50,
+    ],
+    [
+        'home' => 'Milano',
+        'visitors' => 'Napoli',
+        'home-score' => 50,
+        'visitors-score' => 50,
+    ],
+    [
+        'home' => 'Milano',
+        'visitors' => 'Napoli',
+        'home-score' => 50,
+        'visitors-score' => 50,
+    ],
+];
+  ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
@@ -5,41 +39,16 @@
         <title></title>
     </head>
     <body>
-        <?php
-        $matches = [
-            'Partita 1'[
-                'home' => 'Milano',
-                'visitors' => 'Napoli',
-                'home-score' => 50,
-                'visitors-score' => 50,
-            ],
-            'Partita 1'[
-                'home' => 'Milano',
-                'visitors' => 'Napoli',
-                'home-score' => 50,
-                'visitors-score' => 50,
-            ],
-            'Partita 1'[
-                'home' => 'Milano',
-                'visitors' => 'Napoli',
-                'home-score' => 50,
-                'visitors-score' => 50,
-            ],
-            'Partita 1'[
-                'home' => 'Milano',
-                'visitors' => 'Napoli',
-                'home-score' => 50,
-                'visitors-score' => 50,
-            ],
-            'Partita 1'[
-                'home' => 'Milano',
-                'visitors' => 'Napoli',
-                'home-score' => 50,
-                'visitors-score' => 50,
-            ],
-        ];
-          ?>
 
-          <p><?php echo var_dump($matches)  ?></p>
+
+          <p><?php var_dump($matches)  ?></p>
+          <?php array_key_exists(home, $matches) ?>
+          <?php foreach ($matches as $match ){
+
+
+            <h1><?phpecho $match[home] ;</h1>
+        }?>
+
+
     </body>
 </html>
